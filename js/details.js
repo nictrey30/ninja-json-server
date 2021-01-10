a3baaddaa6717b03afb0ffbc98be1c4e3f9b5564
Traian <niculescu.traian.ct@gmail.com>
2021-01-11 01:33:07 +0200

    finished part1

--- js/details.js
+++ js/details.js
@@ -1,1 +1,20 @@
 // javascript for details.html
+// The URLSearchParams interface defines utility methods to work with the query string of a URL.
+const id = new URLSearchParams(window.location.search).get('id');
+const container = document.querySelector('.details');
+
+const renderDetails = async (id) => {
+  let uri = `http://localhost:3000/posts/${id}`;
+  const res = await fetch(uri);
+  const post = await res.json();
+
+  const template = `
+    <h1>${post.title}</h1>
+    <p>${post.body}</p>
+  
+    `;
+
+  container.innerHTML = template;
+};
+
+window.addEventListener('DOMContentLoaded', () => renderDetails(id));
