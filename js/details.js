a90f9b9b1102e0158f1cc4dfff1b5a19c73416e0
Traian <niculescu.traian.ct@gmail.com>
2021-01-11 02:39:01 +0200

    delete button in a blog post

--- js/details.js
+++ js/details.js
@@ -2,6 +2,7 @@
 // The URLSearchParams interface defines utility methods to work with the query string of a URL.
 const id = new URLSearchParams(window.location.search).get('id');
 const container = document.querySelector('.details');
+const deleteBtn = document.querySelector('.delete');
 
 const renderDetails = async (id) => {
   let uri = `http://localhost:3000/posts/${id}`;
@@ -11,10 +12,17 @@ const renderDetails = async (id) => {
   const template = `
     <h1>${post.title}</h1>
     <p>${post.body}</p>
-  
-    `;
+  `;
 
   container.innerHTML = template;
 };
+
+deleteBtn.addEventListener('click', async (e) => {
+  const res = await fetch(`http://localhost:3000/posts/${id}`, {
+    method: 'DELETE'
+  });
+  // relocate the user to the home page after the deletion of the blog post
+  window.location.replace('/');
+});
 
 window.addEventListener('DOMContentLoaded', () => renderDetails(id));
