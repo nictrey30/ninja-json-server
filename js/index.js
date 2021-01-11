ea0c0824776226cbec93a6936efd1b6fe0e3729d
Traian <niculescu.traian.ct@gmail.com>
2021-01-11 03:16:11 +0200

    final files

--- js/index.js
+++ js/index.js
@@ -1,9 +1,15 @@
 // javascript for index.html
 const container = document.querySelector('.blogs');
+const searchFrom = document.querySelector('.search');
 
-const renderPosts = async () => {
+const renderPosts = async (term) => {
   // _sort and _order at the end of the uri is provided by json-server
   let uri = 'http://localhost:3000/posts?_sort=likes&_order=desc';
+  // check if term has a value or not. If it has a value append the uri for search
+  if (term) {
+    // it is handled by the json-server. If it finds the term then it's gonna add that document to the data that is given back
+    uri += `&q=${term}`;
+  }
   const res = await fetch(uri);
   const posts = await res.json();
 
@@ -20,6 +26,11 @@ const renderPosts = async () => {
   });
   container.innerHTML = template;
 };
+
+searchFrom.addEventListener('submit', (e) => {
+  e.preventDefault();
+  renderPosts(e.target.term.value.trim());
+});
 
 // wait to fetch the data until the document has loaded
 // renderPosts is wrapped in an arrow function because I don't wanna get the event object
